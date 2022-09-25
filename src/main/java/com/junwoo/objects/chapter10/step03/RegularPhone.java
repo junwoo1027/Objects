327666145839fa0385a18d59727d0a58829f6e65
kimjunwoo <junwoo1027@naver.com>
2022-09-26 01:52:30 +0900

    공통적인 핵심 로직 추상화

---
+++ src/main/java/com/junwoo/objects/chapter10/step03/RegularPhone.java
@@ -0,0 +1,27 @@
+package com.junwoo.objects.chapter10.step03;
+
+import java.time.Duration;
+
+public class RegularPhone extends Phone {
+    private Money amount;
+    private Duration seconds;
+
+    public RegularPhone(Money amount, Duration seconds, double taxRate) {
+        super(taxRate);
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    public Money getAmount() {
+        return amount;
+    }
+
+    public Duration getSeconds() {
+        return seconds;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
